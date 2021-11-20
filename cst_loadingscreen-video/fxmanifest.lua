c5bb77363776173dfa655078e295f40f437b0d31
COSSART <78325525+COSSART-FR@users.noreply.github.com>
2021-11-20 09:31:30 +0100

    patch bug

--- cst_loadingscreen-video/fxmanifest.lua
+++ cst_loadingscreen-video/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "adamant"
 game "gta5"
 
 author "COSSART"
-description "https://discord.gg/aVpVepauZx"
+description "https://discord.gg/8kWfWU4BXU"
 version "1.0"
 
 files {
@@ -10,6 +10,11 @@ files {
     'css/style.css',
     'css/cursor.css',
     'js/app.js',
+    'js/config.js',
+    'js/core.js',
+    'video/video.mp4',
+    'music/music.mp3',
+    'img/logo.png'
 }
 
 loadscreen 'index.html'
